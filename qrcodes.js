4638e21206d871233b35e3a6aac7988453b704d6
NicChappell <nic.chappell@generalassemb.ly>
2020-07-07 22:02:06 -0600

    updated qrcodes

--- qrcodes.js
+++ qrcodes.js
@@ -1,6 +1,6 @@
 var QRCode = require('qrcode');
 
-QRCode.toFile('./qrcodes/longs-peak.png', 'summitclub.co/r/longs-peak', {
+QRCode.toFile('./qrcodes/longs-peak.png', 'http://www.summitclub.co/r/longs-peak', {
     color: {
         dark: '#000',  // Black dots
         light: '#fff' // White background
