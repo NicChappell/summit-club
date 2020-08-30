0aa42cde8532b2c1f657ca233b44e65f7793fdb2
NicChappell <nic.chappell@generalassemb.ly>
2020-08-30 16:03:44 -0600

    fourteeners routes

--- routes/api/v1/fourteeners.js
+++ routes/api/v1/fourteeners.js
@@ -1,8 +1,8 @@
 // dependencies
 var express = require('express');
 
-// data
-var fourteeners = require('../../../data/fourteeners.json');
+// models
+var db = require('../../../models')
 
 // new router
 var router = express.Router();
@@ -10,17 +10,25 @@ var router = express.Router();
 // @route:  GET /api/fourteeners
 // @desc:   Select all fourteener records
 router.get('/', function (req, res) {
-    res.json(fourteeners);
+    db.Fourteener.find({})
+        .then(result => res.status(200).json(result))
+        .catch(err => res.status(500).json(err));
+});
+
+// @route:  POST /api/fourteeners
+// @desc:   Create new fourteener records
+router.post('/', function (req, res) {
+    db.Fourteener.create(req.body)
+        .then(result => res.status(200).json(result))
+        .catch(err => res.status(500).json(err));
 });
 
 // @route:  GET /api/fourteeners/:slug
 // @desc:   Select single fourteener record
 router.get('/:slug', function (req, res) {
-    var fourteener = fourteeners.filter(fourteener => {
-        return fourteener.slug === req.params.slug;
-    });
-
-    res.json(fourteener[0]);
+    db.Fourteener.find({ slug: req.params.slug })
+        .then(result => res.status(200).json(result))
+        .catch(err => res.status(500).json(err));
 });
 
 module.exports = router;
